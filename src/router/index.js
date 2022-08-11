4ac58d8210ce35654f32af7e7c9cfb2622dad10c
iJulyke <759811792@qq.com>
2022-08-11 18:59:39 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/components/Login.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  
-  
-]
-
 const router = new VueRouter({
-  routes
+  routes :[
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path:'/',
+      redirect:'/login'
+    }
+  ]
 })
 
 export default router
