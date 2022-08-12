7645b3fc50d6c3726124b7daa33ad84bac195649
iJulyke <759811792@qq.com>
2022-08-12 21:14:07 +0800

    updata login

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios//每一个vue组件都可以通过http直接发起ajax请求
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/components/Login.vue'
+import Home from '@/components/Home.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes :[
     {
+      path:'/',
+      redirect:'/login'
+    },
+    {
       path:'/login',
       component:Login
     },
     {
-      path:'/',
-      redirect:'/login'
+      path:'/home',
+      component:Home
     }
   ]
 })
+router.beforeEach((to,from,next)=>{
+  // to将要访问的路径
+  // from代表从哪个路径跳转而来
+  // next是一个函数，表示放行
+  // next（）放行  next('/login)强制跳转
 
+  const token = window.sessionStorage.getItem('token')
+  if(to.path === '/login'){return next()}
+  if(!token){
+    console.log('token不存在')
+    return next('/login')
+  }
+  next()
+})
 export default router
